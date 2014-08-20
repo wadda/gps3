e7ca20b915ef8ca8ddcbfd5854870cc4fabdb09e
SyPy Hacknight <moe@wadda.org>
2014-08-20 12:40:00 +0000

    refactored and replaced custom functions with standard library

--- misc.py
+++ misc.py
@@ -3,7 +3,9 @@
 # This file is Copyright (c) 2010 by the GPSD project
 # BSD terms apply: see the file COPYING in the distribution root for details.
 
-import time, calendar, math
+import time
+import calendar
+from math import sin, cos, acos, radians
 
 # some multipliers for interpreting GPS output
 METERS_TO_FEET = 3.2808399  # Meters to U.S./British feet
@@ -18,18 +20,9 @@ MPS_TO_KNOTS = 1.9438445  # Meters per second to knots
 
 # EarthDistance code swiped from Kismet and corrected
 
-def Deg2Rad(x):
-    "Degrees to radians."
-    return x * (math.pi / 180)
-
-
-def Rad2Deg(x):
-    "Radians to degress."
-    return x * (180 / math.pi)
-
 
 def CalcRad(lat):
-    "Radius of curvature in meters at specified latitude."
+    """Radius of curvature in meters at specified latitude."""
     a = 6378.137
     e2 = 0.081082 * 0.081082
     # the radius of curvature of an ellipsoidal Earth in the plane of a
@@ -44,24 +37,24 @@ def CalcRad(lat):
     # a = 6378 km (3963 mi) Equatorial radius (surface to center distance)
     # b = 6356.752 km (3950 mi) Polar radius (surface to center distance)
     # e = 0.081082 Eccentricity
-    sc = math.sin(Deg2Rad(lat))
+    sc = sin(radians(lat))
     x = a * (1.0 - e2)
     z = 1.0 - e2 * sc * sc
     y = pow(z, 1.5)
     r = x / y
 
-    r = r * 1000.0  # Convert to meters
+    r *= 1000.0  # Convert to meters
     return r
 
 
-def EarthDistance((lat1, lon1), (lat2, lon2)):
-    "Distance in meters between two points specified in degrees."
-    x1 = CalcRad(lat1) * math.cos(Deg2Rad(lon1)) * math.sin(Deg2Rad(90 - lat1))
-    x2 = CalcRad(lat2) * math.cos(Deg2Rad(lon2)) * math.sin(Deg2Rad(90 - lat2))
-    y1 = CalcRad(lat1) * math.sin(Deg2Rad(lon1)) * math.sin(Deg2Rad(90 - lat1))
-    y2 = CalcRad(lat2) * math.sin(Deg2Rad(lon2)) * math.sin(Deg2Rad(90 - lat2))
-    z1 = CalcRad(lat1) * math.cos(Deg2Rad(90 - lat1))
-    z2 = CalcRad(lat2) * math.cos(Deg2Rad(90 - lat2))
+def EarthDistance(lat1, lon1, lat2, lon2):
+    """Distance in meters between two points specified in degrees."""
+    x1 = CalcRad(lat1) * cos(radians(lon1)) * sin(radians(90 - lat1))
+    x2 = CalcRad(lat2) * cos(radians(lon2)) * sin(radians(90 - lat2))
+    y1 = CalcRad(lat1) * sin(radians(lon1)) * sin(radians(90 - lat1))
+    y2 = CalcRad(lat2) * sin(radians(lon2)) * sin(radians(90 - lat2))
+    z1 = CalcRad(lat1) * cos(radians(90 - lat1))
+    z2 = CalcRad(lat2) * cos(radians(90 - lat2))
     a = (x1 * x2 + y1 * y2 + z1 * z2) / pow(CalcRad((lat1 + lat2) / 2), 2)
     # a should be in [1, -1] but can sometimes fall outside it by
     # a very small amount due to rounding errors in the preceding
@@ -71,28 +64,30 @@ def EarthDistance((lat1, lon1), (lat2, lon2)):
         a = 1
     elif a < -1:
         a = -1
-    return CalcRad((lat1 + lat2) / 2) * math.acos(a)
+    return CalcRad((lat1 + lat2) / 2) * acos(a)
 
 
-def MeterOffset((lat1, lon1), (lat2, lon2)):
-    "Return offset in meters of second arg from first."
-    dx = EarthDistance((lat1, lon1), (lat1, lon2))
-    dy = EarthDistance((lat1, lon1), (lat2, lon1))
-    if lat1 < lat2: dy *= -1
-    if lon1 < lon2: dx *= -1
-    return (dx, dy)
+def MeterOffset(lat1, lon1, lat2, lon2):
+    """Return offset in meters of second arg from first."""
+    dx = EarthDistance(lat1, lon1, lat1, lon2)
+    dy = EarthDistance(lat1, lon1, lat2, lon1)
+    if lat1 < lat2:
+        dy *= -1
+    if lon1 < lon2:
+        dx *= -1
+    return dx, dy
 
 
 def isotime(s):
-    "Convert timestamps in ISO8661 format to and from Unix time."
-    if type(s) == type(1):
+    """Convert timestamps in ISO8661 format to and from Unix time."""
+    if isinstance(s, type(1)):
         return time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(s))
-    elif type(s) == type(1.0):
+    elif isinstance(s, type(1.0)):
         date = int(s)
         msec = s - date
         date = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(s))
         return date + "." + repr(msec)[3:]
-    elif type(s) == type("") or type(s) == type(u""):
+    elif isinstance(s, type("")) or isinstance(s, type(u"")):
         if s[-1] == "Z":
             s = s[:-1]
         if "." in s:
@@ -100,10 +95,10 @@ def isotime(s):
         else:
             date = s
             msec = "0"
-        # Note: no leap-second correction! 
-        return calendar.timegm(time.strptime(date, "%Y-%m-%dT%H:%M:%S")) + float("0." + msec)
+        # Note: no leap-second correction!
+        return calendar.timegm(
+            time.strptime(date, "%Y-%m-%dT%H:%M:%S")) + float("0." + msec)
     else:
         raise TypeError
 
 # End
-
