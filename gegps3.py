c6db65bfc7314cb4a77853a35795781b14a5b96f
SyPy <sypy@wadda.org>
2014-11-19 13:34:24 +1100

    Demo that creates google earth kml file (/tmp/gps3_live.kml) for realtime (4 second) updates  Fixed interrupt

--- gegps3.py
+++ gegps3.py
@@ -14,21 +14,22 @@ the_fix = gps3.Fix()
 the_link = '/tmp/gps3_live.kml'
 the_file = '/tmp/gps3_static.kml'
 
-output_repeater = """<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
-<NetworkLink>
-    <name>GPS3 Live</name>
-    <Link>
-        <href>{0}</href>
-        <refreshMode>onInterval</refreshMode>
-    </Link>
-</NetworkLink>
-</kml>""".format(the_file)
+output_repeater = ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                   "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n"
+                   "<NetworkLink>\n"
+                   "    <name>GPS3 Live</name>\n"
+                   "    <Link>\n"
+                   "        <href>{0}</href>\n"
+                   "        <refreshMode>onInterval</refreshMode>\n"
+                   "    </Link>\n"
+                   "</NetworkLink>\n"
+                   "</kml>"
+).format(the_file)
 f = open(the_link, 'w')
 f.write(output_repeater)
 f.close()
 
-while 1:
+try:
     for new_data in the_connection:
         if new_data:
             the_fix.refresh(new_data)
@@ -38,23 +39,24 @@ while 1:
             longitude = the_fix.TPV['lon']
             altitude = the_fix.TPV['alt']
 
-            output_instance = """<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://earth.google.com/kml/2.0">
-    <Placemark>
-        <name>{0} km/h</name>
-        <description></description>
-        <LookAt>
-            <longitude>{1}</longitude>
-            <latitude>{2}</latitude>
-            <range>600</range>
-            <tilt>30</tilt>
-            <heading>0</heading>
-        </LookAt>
-        <Point>
-            <coordinates>{1},{2},{3}</coordinates>
-        </Point>
-    </Placemark>
-    </kml>""".format(speed, longitude, latitude, altitude)
+            output_instance = ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                               "<kml xmlns=\"http://earth.google.com/kml/2.0\">\n"
+                               "    <Placemark>\n"
+                               "        <name>{0} km/h</name>\n"
+                               "        <description></description>\n"
+                               "        <LookAt>\n"
+                               "            <longitude>{1}</longitude>\n"
+                               "            <latitude>{2}</latitude>\n"
+                               "            <range>600</range>\n"
+                               "            <tilt>30</tilt>\n"
+                               "            <heading>0</heading>\n"
+                               "        </LookAt>\n"
+                               "        <Point>\n"
+                               "            <coordinates>{1},{2},{3}</coordinates>\n"
+                               "        </Point>\n"
+                               "    </Placemark>\n"
+                               "</kml>"
+            ).format(speed, longitude, latitude, altitude)
 
             f = open(the_file, 'w')
             f.write(output_instance)
@@ -64,7 +66,7 @@ while 1:
             pass
         time.sleep(1)
 
-    except KeyboardInterrupt:
-        the_connection.close()
-        print("\nTerminated by user\nGood Bye.\n")
+except KeyboardInterrupt:
+    the_connection.close()
+    print("\nTerminated by user\nGood Bye.\n")
 #End
