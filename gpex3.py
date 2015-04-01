f4fd16dd9cc81007c515f950fe85e06f2483f26b
moe <deadcanary@wadda.org>
2015-04-01 12:25:51 +1100

    Learning 	deleted:    $APP_CONFIG$/CodeGlance.xml 	deleted:    $APP_CONFIG$/CodeOutlinePlugin.xml 	deleted:    $APP_CONFIG$/code.style.schemes.xml 	deleted:    $APP_CONFIG$/colors.scheme.xml 	deleted:    $APP_CONFIG$/customization.xml 	deleted:    $APP_CONFIG$/eclipseCodeFormatter.xml 	deleted:    $APP_CONFIG$/editor.codeinsight.xml 	deleted:    $APP_CONFIG$/editor.rich.copy.xml 	deleted:    $APP_CONFIG$/editor.xml 	deleted:    $APP_CONFIG$/emmet.xml 	deleted:    $APP_CONFIG$/file.template.settings.xml 	deleted:    $APP_CONFIG$/filetypes.xml 	deleted:    $APP_CONFIG$/find.xml 	deleted:    $APP_CONFIG$/gisttemplates_settings.xml 	deleted:    $APP_CONFIG$/github_settings.xml 	deleted:    $APP_CONFIG$/ide.general.xml 	deleted:    $APP_CONFIG$/ignore.xml 	deleted:    $APP_CONFIG$/markdown.xml 	deleted:    $APP_CONFIG$/notifications.xml 	deleted:    $APP_CONFIG$/options.xml 	deleted:    $APP_CONFIG$/other.xml 	deleted:    $APP_CONFIG$/packages.xml 	deleted:    $APP_CONFIG$/project.default.xml 	deleted:    $APP_CONFIG$/proxy.settings.xml 	deleted:    $APP_CONFIG$/smartereditor.xml 	deleted:    $APP_CONFIG$/template.settings.xml 	deleted:    $APP_CONFIG$/ui.lnf.xml 	deleted:    $APP_CONFIG$/vcs.xml 	deleted:    $APP_CONFIG$/web-browsers.xml 	deleted:    $APP_CONFIG$/xStructure.xml 	deleted:    $ROOT_CONFIG$/codestyles/Default _1_.xml 	deleted:    $ROOT_CONFIG$/colors/Darcula copy.icls 	deleted:    $ROOT_CONFIG$/colors/Github copy.icls 	deleted:    $ROOT_CONFIG$/colors/warmeon.icls 	deleted:    $ROOT_CONFIG$/filetypes/conky.xml 	deleted:    $ROOT_CONFIG$/inspection/Default.xml 	deleted:    $ROOT_CONFIG$/inspection/__deleted.xml 	deleted:    $ROOT_CONFIG$/quicklists/Deployment.xml 	deleted:    $ROOT_CONFIG$/tools/External Tools.xml 	deleted:    _linux/$APP_CONFIG$/git.xml 	deleted:    _linux/$APP_CONFIG$/keymap.xml 	deleted:    _linux/$APP_CONFIG$/laf.xml 	modified:   gpex3.py 	deleted:    gepeex.py

--- gpex3.py
+++ gpex3.py
@@ -1,87 +1,21 @@
-#! /usr/bin/python3
-# coding=utf-8
-"""banana"""
-import xml.dom.minidom
-import gps3
-import time
-from datetime import datetime, timezone, timedelta
-import os
-import sys
-
-gps_connection = gps3.GPSDSocket()
-gps_fix = gps3.Fix()
+#!/usr/bin/env python
+""" banana """ # <-- For scale
 
 
-the_log = '/tmp/gpx3.gpx'
+__package__ =
+__packages__ = ,
+__author__ = \"moe\"
+__copyright__ = "Copyright 2015, Wadda Big Deal"
+__license__ = "MIT"
+__version__ = "0.1a"
+__email__ = "waddagit@wadda.org"
+__status__ = "Flammable"
+__created__ = 2015/04/01
 
 
-def start_time():
-    """time in the beginning"""
-    timestart = str(datetime.utcnow().replace(tzinfo=(timezone(timedelta(0)))))
-    return timestart
 
-if os.path.isfile(the_log):
-    doc = minidom.parse(the_log)  # opens the pre-existing
-    # gpx_element = doc.firstChild
-    # create_element(trk)
-    # track = gpx3.createElementNS(None, 'trk')
 
-else:
-    doc = xml.dom.minidom.Document()
-    gpx_element = doc.createElement("gpx")
-    doc.appendChild(gpx_element)
-
-trk_element = doc.createElement("trkseg")
-trk_element.setAttribute("began", start_time())
-gpx_element.appendChild(trk_element)
-utc = alt = hdop = vdop = pdop = mode = sats = tag = 'n/a'
-try:
-    tpv_list = {'time': utc, 'ele': alt, 'tag': tag}
-    sky_list = {'hdop': hdop, 'vdop': vdop, 'pdop': pdop}
-    # misc_list = {'sat': sats, 'fix':mode}
-    element = {}
-
-    for new_data in gps_connection:
-        if new_data:
-            gps_fix.refresh(new_data)
-        if not isinstance(gps_fix.TPV['lat'], str):
-            trkpt_element = doc.createElement("trkpt")
-            trk_element.appendChild(trkpt_element)
-            trkpt_element.setAttribute('lat', str(gps_fix.TPV['lat']))
-            trkpt_element.setAttribute('lon', str(gps_fix.TPV['lon']))
-
-            # for latlon in ('lat', 'lon'):
-            #     trkpt_element.setAttribute(latlon, str(gps_fix.TPV[latlon]))
-
-            #tp_list[key] =
-            for key in tpv_list:
-                if key == 'ele':
-                    element[key] = '{}'.format(gps_fix.TPV['alt'])  # because consistency with labels is a horrible.
-                else:
-                    element[key] = '{}'.format(gps_fix.TPV[key])
-            for key in sky_list:
-                element[key] = '{}'.format(gps_fix.SKY[key])
-            # Misc.
-            element['sat'] = '{}'.format(gps_fix.satellites_used()[1])
-
-            # tp_list['ele'] = '{}'.format(gps_fix.TPV['alt'])
-            # tp_list['hdop'] = '{}'.format(gps_fix.SKY['hdop'])
-            # tp_list['vdop'] = '{}'.format(gps_fix.SKY['vdop'])
-            # tp_list['pdop'] = '{}'.format(gps_fix.SKY['pdop'])
-            # tp_list['fix'] = '{}'.format(("ZERO", "NO_FIX", "2D", "3D")[gps_fix.TPV['mode']])
-            # tp_list['sat'] = '{0[1]}'.format(gps_fix.satellites_used())
-            # tp_list['tag'] = '{}'.format(gps_fix.TPV['tag'])
-
-            for key in element:
-                trkpt_data = doc.createElement(key)
-                trkpt_element.appendChild(trkpt_data)
-
-                new_value = doc.createTextNode(element[key])
-                trkpt_data.appendChild(new_value)
-
-            print(doc.toprettyxml())
-            time.sleep(1)
-
-except KeyboardInterrupt:
-    gps_connection.close()
-    print("\nTerminated by user\nGood Bye.\n")
+if __name__ == '__main__':    # code to execute if called from command-line
+    pass    # do nothing, unless code is added to this template
+            # this is either a simple example of how to use the module,
+            # or when the module can meaningfully be called as a script.
